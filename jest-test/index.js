dac3537827ae6ecc25cb5e6d9747e8594a9d4655
sonsu0710 <sonsu0710@gmail.com>
2022-12-27 03:14:59 +0900

    fisrt commit

---
+++ jest-test/index.js
@@ -0,0 +1,16 @@
+const sum = (a, b) => {
+    return a + b;
+};
+
+const person = (name, age) => {
+    return {
+        name,
+        age,
+    };
+};
+
+const toggle = (a) => {
+    return !a;
+}
+
+module.exports = { sum, person, toggle, };
---
+++ jest-test/index.test.js
@@ -0,0 +1,23 @@
+const {sum, person, toggle} = require('./index');
+
+describe('test index.js file', () => {
+
+    // Matcher 사용
+    // Matcher 는 결과값을 비교하여 테스트의 성공 여부를 파악.
+
+    // toBe 메서드 사용
+    it('sums 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+
+    // toEqual 메서드 사용
+    it('make a person', () => {
+        expect(person('Kim', 20)).toEqual({
+            name: 'Kim',
+            age: 20,
+        });
+    });
+
+    // toBeFalsy, toBeTruthy 사용
+
+});
